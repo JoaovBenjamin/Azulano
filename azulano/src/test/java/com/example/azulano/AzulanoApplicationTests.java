83f1c91c08068907522c055a8773b340e0e8fde0
Joaovvb <rm98938@fiap.com.br>
2024-06-07 20:35:26 -0300

    aaa

--- azulano/src/test/java/com/example/azulano/AzulanoApplicationTests.java
+++ azulano/src/test/java/com/example/azulano/AzulanoApplicationTests.java
@@ -3,8 +3,8 @@ package com.example.azulano;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PokedexApplicationTests {
+@SpringBootTest()
+class AzulanoApplicationTests {
 
 	@Test
 	void contextLoads() {
